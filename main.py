87d1797ad6ae03adee17bd3574a944eb7e71ae8c
tyjaz19 <tyjazynka@proton.me>
2024-09-14 19:10:15 -0400

    add fps 60

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-
 from constants import *
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+
+
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
